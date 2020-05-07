9e1c37645400b6fc2678a98d4547bcff3011fbaf
Sean Holcomb <seanholcomb@gmail.com>
2020-05-07 00:25:35 -0700

    add file

---
+++ src/test/java/com/holcomb/sean/recipeapp/RecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.holcomb.sean.recipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
