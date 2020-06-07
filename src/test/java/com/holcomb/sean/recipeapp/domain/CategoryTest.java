0703edd4059b25a1c44ab36c18e1b84aaaec58c7
Sean Holcomb <seanholcomb@gmail.com>
2020-06-07 13:38:52 -0700

    Add tests

---
+++ src/test/java/com/holcomb/sean/recipeapp/domain/CategoryTest.java
@@ -0,0 +1,36 @@
+package com.holcomb.sean.recipeapp.domain;
+
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class CategoryTest {
+
+    Category category;
+
+    @BeforeAll
+    public void setUp() {
+        category = new Category();
+    }
+
+    @Test
+    void getId() {
+        Long idValue = 4L;
+
+        category.setId(idValue);
+
+        assertEquals(idValue, category.getId());
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void getRecipes() {
+    }
+}
---
+++ src/test/java/com/holcomb/sean/recipeapp/services/RecipeServiceImplTest.java
@@ -0,0 +1,46 @@
+package com.holcomb.sean.recipeapp.services;
+
+import com.holcomb.sean.recipeapp.domain.Recipe;
+import com.holcomb.sean.recipeapp.repositories.RecipeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.stubbing.OngoingStubbing;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    void getRecipes() {
+        Recipe recipe = new Recipe();
+        HashSet<Recipe> recipesData = new HashSet<>();
+        recipesData.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(recipesData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+
+        assertEquals(recipes.size(), 1);
+        verify(recipeRepository, times(1)).findAll();
+    }
+
+
+}
