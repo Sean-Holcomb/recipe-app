83472020986a5d1edc9753f8eb1c624df6b08a30
Sean Holcomb <seanholcomb@gmail.com>
2020-06-12 21:03:16 -0700

    Add Recipe Controller

--- src/main/java/com/holcomb/sean/recipeapp/controllers/IndexController.java
+++ src/main/java/com/holcomb/sean/recipeapp/controllers/IndexController.java
@@ -29,4 +29,5 @@ private final RecipeService recipeService;
         return "index";
     }
 
+
 }
--- src/test/java/com/holcomb/sean/recipeapp/services/RecipeServiceImplTest.java
+++ src/test/java/com/holcomb/sean/recipeapp/services/RecipeServiceImplTest.java
@@ -9,6 +9,7 @@ import org.mockito.MockitoAnnotations;
 import org.mockito.stubbing.OngoingStubbing;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -29,17 +30,34 @@ class RecipeServiceImplTest {
     }
 
     @Test
-    void getRecipes() {
+    public void getRecipeByIdTest() throws Exception {
         Recipe recipe = new Recipe();
-        HashSet<Recipe> recipesData = new HashSet<>();
-        recipesData.add(recipe);
+        recipe.setId(1L);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
 
-        when(recipeRepository.findAll()).thenReturn(recipesData);
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        Recipe recipeReturned = recipeService.findById(1L);
+
+        assertNotNull(recipeReturned);
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository, never()).findAll();
+    }
+
+    @Test
+    public void getRecipesTest() throws Exception {
+
+        Recipe recipe = new Recipe();
+        HashSet receipesData = new HashSet();
+        receipesData.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(receipesData);
 
         Set<Recipe> recipes = recipeService.getRecipes();
 
         assertEquals(recipes.size(), 1);
         verify(recipeRepository, times(1)).findAll();
+        verify(recipeRepository, never()).findById(anyLong());
     }
 
 
